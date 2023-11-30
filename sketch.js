e2c655c3a1c705194732ed4fec1c14977fcc0a60
santiagoc1106 <santiagocastaneda920@outlook.com>
2023-11-30 10:28:37 -0500

    first

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
+var xoff = 0;
+var yoff = 0;
+
+
 function setup() {
   createCanvas(700, 700);
 }
 
 function draw() {
-  background(200);
+  background(51);
+
+
+  var x = map(noise(xoff), 0, 1, 0, width);
+  var y = map(nosie(xoff), 0, 1, 0, height);
+
+  xoff += 0.02;
+
+  ellipse (x, y, 24, 24);
 }
