6b96dbf0beea23ab0456eda6011d54d570ae4567
Your Name <you@example.com>
2023-12-13 21:35:00 -0500

    progress

--- sketch.js
+++ sketch.js
@@ -1,19 +1,42 @@
-var xoff = 0;
-var yoff = 0;
 
+let inc = 0.01;
+let start = 0;
 
 function setup() {
   createCanvas(700, 700);
 }
 
 function draw() {
-  background(51);
+  background(135, 206, 235);
+  stroke(255);
+  beginShape();
+  let xoff = start;
+  for (let x = 0; x < width; x+=0.75) {
+    
+   noStroke();
+   noFill();
+    let y = noise(xoff) * 800;
+    vertex(x, y);
+    xoff += .007;
+  }
+  fill(50);
+  endShape();
+
+  start += inc/2;
+  var trunk = 0;
+
+  noStroke();
+  for(trunk = 10; trunk < 800; trunk +=50){
+    fill(64, 44, 5);
+    rect(trunk,550,40,332);
+  }
+
+  var leaves=10;
+  for(leaves=30;leaves<800;leaves+=50){
+    fill(72, 110, 12);
+    ellipse(leaves,520,127,116);
+  }
+  stroke(250);
 
 
-  var x = map(noise(xoff), 0, 1, 0, width);
-  var y = map(nosie(xoff), 0, 1, 0, height);
-
-  xoff += 0.02;
-
-  ellipse (x, y, 24, 24);
-}
+} 
